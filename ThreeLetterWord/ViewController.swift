1b2b5e95edff562b6edebb3f0f23b2a38500253b
Severin Hermanson <shermanson0141@stu.d214.org>
2020-01-07 11:06:34 -0600

    Initial Commit

---
+++ ThreeLetterWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThreeLetterWord
+//
+//  Created by Severin on 1/7/20.
+//  Copyright © 2020 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
